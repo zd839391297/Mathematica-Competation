2d64fa0fa1751d40723296d907342752d9f35a11
zd839391297 <zd839391297@outlook.com>
2018-11-02 19:20:42 +0800

    v2.0
    practice

--- Python.py
+++ Python.py
@@ -1,1 +1,16 @@
-print("hello,world")
+# print("hello,world")
+
+import numpy as np
+from scipy import integrate
+import math
+
+
+def f(x, y):
+    return math.sin(x)*y
+
+
+#a = integrate.quad(f, 0, math.pi/2)
+#print(a)
+
+b = integrate.dblquad(f,0, 1, lambda x: 0, lambda x: x+1)
+print(b)
